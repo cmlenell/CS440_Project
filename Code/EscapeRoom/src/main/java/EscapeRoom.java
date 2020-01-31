fd39fa3061ef75d541eb78b6da6109b988e3a4c3
Deonvell Shed <deonvellshed@gmail.com>
2020-01-31 11:48:34 -0600

    Maven Project of Escape Room
    The basic java maven for the application

---
+++ Code/EscapeRoom/src/main/java/EscapeRoom.java
@@ -0,0 +1,14 @@
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class EscapeRoom extends Application {
+    private Stage primaryStage;
+
+    public void start(Stage primaryStage) throws Exception {
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
