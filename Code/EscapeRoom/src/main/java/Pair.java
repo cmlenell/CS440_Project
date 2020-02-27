997a4ae732edaa1ab1c452ed69fe3ab7528ef56f
Deonvell Shed <deonvellshed@gmail.com>
2020-02-26 19:40:49 -0600

    Current Game with sprities and moving floor
    This is the version of the game that has the static floor and spirites that work. Still need to figure out how to make the key listerner stop when an popup comes up

---
+++ Code/EscapeRoom/src/main/java/Pair.java
@@ -0,0 +1,19 @@
+public class Pair  {
+    public Double first;
+    private Integer  second;
+    Pair(Double first, Integer second){
+
+        this.first = first;
+        this.second = second;
+
+    }
+    public Double  getFirst(){
+        return first;
+    }
+    public Integer getSecond(){
+        return second;
+    }
+
+
+
+}
